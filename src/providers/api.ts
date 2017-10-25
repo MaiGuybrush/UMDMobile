771fbb0db6f0730b600a015123841a93877f8ac2
Guy Mai <guy.mai@innolux.com>
2017-10-25 08:18:43 +0800

    刪除apiConnect 判斷

--- src/providers/api.ts
+++ src/providers/api.ts
@@ -67,16 +67,16 @@ export class Api {
   static getHttpUrl(funcName: string): any {
      let apiConnect: string = "CIM";  //MIS,CIM
      let serverUrl: string = "";
-     if (apiConnect === "CIM"){
+    //  if (apiConnect === "CIM"){
         // serverUrl = "http://c4c010685.cminl.oa/UMD/Services/UMDDataService.svc/";
       // serverUrl = "http://tnvtwebapi.cminl.oa/NewWebApi/Agency/api/service/c18ac7b5-bbfa-8919-eb8e-b3fe20e2ac93/";
         // serverUrl = "http://c3c003309.cminl.oa/UMD/Services/UMDDataService.svc/";
-         serverUrl = "https://apptest.innolux.com/agency/api/proxy/c18ac7b5-bbfa-8919-eb8e-b3fe20e2ac93/";
-        //  serverUrl = "https://inxapp2.innolux.com/agency/api/proxy/c18ac7b5-bbfa-8919-eb8e-b3fe20e2ac93/";
-        //  serverUrl = "https://inxapp2.innolux.com/agency/api/proxy/c18ac7b5-bbfa-8919-eb8e-b3fe20e2ac93/";
-     }else if (apiConnect === "MIS"){
-      serverUrl = "http://ptnecimumd.cminl.oa/Services/UMDDataService.svc/";
-     }
+          serverUrl = "https://apptest.innolux.com/agency/api/proxy/c18ac7b5-bbfa-8919-eb8e-b3fe20e2ac93/";
+          //serverUrl = "http://c7c002811.cminl.oa:8080/agency/api/proxy/c18ac7b5-bbfa-8919-eb8e-b3fe20e2ac93/";
+          //serverUrl = "https://jnapp02/WebApi/Agency/cbf3b47d-115b-a97c-62c8-40dc52b6b1c7"
+      // }else if (apiConnect === "MIS"){
+      //serverUrl = "http://ptnecimumd.cminl.oa/Services/UMDDataService.svc/";
+    //  }
      return serverUrl + funcName;
   }
 
